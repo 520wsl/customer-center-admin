a8e614d0b1d7a55ed75d30ffe87632d67c957039
xzx <xzx>
2018-11-08 16:58:13 +0800

    fix解决冲突

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
-import "@/libs/flexible";
+import "./libs/flexible";
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
-import Mint from "mint-ui";
-import "mint-ui/lib/style.css";
-Vue.use(Mint);
+Vue.use(MintUI)
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-	router,
-	store,
-	render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
