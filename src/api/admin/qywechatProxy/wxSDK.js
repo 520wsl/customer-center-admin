c4ac22e78b13c83e43d7b062012ab21c17a2c541
520wsl <395548460@qq.com>
2018-11-29 09:50:10 +0800

    fix:微信sdk调试

---
+++ src/api/admin/qywechatProxy/wxSDK.js
@@ -0,0 +1,13 @@
+import api from "@/libs/api.request";
+const baseUrl = "/qywechat-proxy";
+function post(url, params) {
+	return api.post(baseUrl + url, params);
+}
+function get(url, params) {
+	return api.get(baseUrl + url, params);
+}
+
+// 企业微信  获取 微信 jsapi SDK config 配置
+export const getQyWxJSSDKConfig = ({ url }) => {
+	return get("/wx/WxJSSDKConfig", { url });
+};
