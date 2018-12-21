61084505db981be2900815a924dbd1cdc1dcc280
2355456062@qq.com <2355456062@qq.com>
2018-12-21 09:36:28 +0800

    fix：优化代码

--- src/admin/view/workOrder/leaderList.vue
+++ src/admin/view/workOrder/leaderList.vue
@@ -384,6 +384,7 @@ export default {
                         //         )
                         //     );
                         // }
+                        
                         btnGroup.push(
                             h(
                                 "a",
