855f746c670f04e2be0e0ce7fec81f23ee8f26c4
520wsl <395548460@qq.com>
2018-12-28 15:39:04 +0800

    fix:案例库、案例库工单列表

---
+++ src/admin/view/case/workOrderCaseList.vue
@@ -0,0 +1,299 @@
+<template>
+    <div>
+        <Card class="md-card">
+            <Table border :columns="columns1" :loading="loading" :data="data1"></Table>
+        </Card>
+        <Card class="md-card">
+            <page
+                :pageNum="params.pageNum"
+                :pageSize="params.pageSize"
+                :count="params.count"
+                @pageCurrentChange="pageCurrentChange"
+                @pageSizeChange="pageSizeChange"
+            ></page>
+        </Card>
+    </div>
+</template>
+
+<script>
+import {
+    getWorkOrderCaseListData,
+    delWorkOrderCaseListData
+} from "@/api/admin/case/case";
+import { formatTime } from "@/libs/util/time";
+import Page from "_c/admin/page";
+export default {
+    components: {
+        Page
+    },
+    mounted() {
+        this.getWorkOrderCaseList();
+    },
+    methods: {
+        delWorkOrderCase(id) {
+            console.log("delWorkOrderCase", id);
+            this.delWorkOrderCaseList(id);
+        },
+        // 查询数据 分页页码重置
+        sleectTemplateList(pageNum) {
+            this.params.pageNum = pageNum;
+            this.getWorkOrderCaseList();
+        },
+        // 设置分页页码
+        pageCurrentChange(pageNum) {
+            this.sleectTemplateList(pageNum);
+        },
+        // 设置分页大小
+        pageSizeChange(pageSize) {
+            this.params.pageSize = pageSize;
+            this.sleectTemplateList(1);
+        },
+        // 获取列表数据
+        async getWorkOrderCaseList() {
+            let res = await getWorkOrderCaseListData({ ...this.params });
+            res.catch(error => {
+                console.log("getWorkOrderCaseList", error);
+                this.$Modal.error({
+                    title: "工单案例库",
+                    content: error.msg
+                });
+                return;
+            });
+
+            this.data1 = res.list || [];
+            this.params.count = res.count || 0;
+        },
+        async delWorkOrderCaseList(caseLibraryId) {
+            let res = await delWorkOrderCaseListData({ caseLibraryId });
+            res.catch(error => {
+                this.$Modal.error({
+                    title: "删除工单案例库",
+                    content: error.msg
+                });
+                return;
+            });
+            this.$Modal.success({
+                title: "删除工单案例库",
+                content: "删除成功"
+            });
+        }
+    },
+    data() {
+        return {
+            columns1: [
+                {
+                    title: "案例名称",
+                    key: "name",
+                    render: (h, params) => {
+                        let btnGroup = [];
+
+                        btnGroup.push(
+                            h(
+                                "a",
+                                {
+                                    style: {
+                                        color: "#2d8cf0",
+                                        display: "block",
+                                        margin: "5px"
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.$router.push({
+                                                name: "case-workOrderCaseInfo",
+                                                params: {
+                                                    id: params.row.id
+                                                }
+                                            });
+                                        }
+                                    }
+                                },
+                                params.row.name
+                            )
+                        );
+
+                        return h("div", btnGroup);
+                    }
+                },
+                {
+                    title: "工单数量",
+                    key: "workOrderNum",
+                    width: 120,
+                    align: "center"
+                },
+                {
+                    title: "分享次数",
+                    key: "shareNum",
+                    width: 120,
+                    align: "center"
+                },
+                {
+                    title: "上次修改时间",
+                    align: "center",
+                    render: (h, params) => {
+                        let btnGroup = [];
+
+                        btnGroup.push(
+                            h(
+                                "span",
+                                {},
+                                formatTime(
+                                    params.row.updateAt,
+                                    "YYYY-MM-DD HH:mm:ss"
+                                )
+                            )
+                        );
+                        return h("div", btnGroup);
+                    }
+                },
+                {
+                    title: "创建人",
+                    key: "userName",
+                    width: 200,
+                    align: "center",
+                    render: (h, params) => {
+                        let btnGroup = [];
+
+                        btnGroup.push(
+                            h(
+                                "span",
+                                {},
+                                params.row.userName +
+                                    " ( " +
+                                    params.row.departmentName +
+                                    " ) "
+                            )
+                        );
+
+                        return h("div", btnGroup);
+                    }
+                },
+                {
+                    title: "操作",
+                    key: "num",
+                    render: (h, params) => {
+                        let btnGroup = [];
+
+                        btnGroup.push(
+                            h(
+                                "Button",
+                                {
+                                    props: {
+                                        type: "primary",
+                                        size: "small",
+                                        ghost: true
+                                    },
+                                    style: {
+                                        margin: "5px"
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.$router.push({
+                                                name: "case-workOrderCaseInfo",
+                                                params: {
+                                                    id: params.row.id
+                                                },
+                                                query: {
+                                                    name: params.row.name
+                                                }
+                                            });
+                                        }
+                                    }
+                                },
+                                "查看案例"
+                            )
+                        );
+
+                        btnGroup.push(
+                            h(
+                                "Poptip",
+                                {
+                                    props: {
+                                        confirm: true,
+                                        title: "你确定要删除吗?"
+                                    },
+
+                                    on: {
+                                        "on-ok": () => {
+                                            this.delWorkOrderCase(
+                                                params.row.id
+                                            );
+                                        }
+                                    }
+                                },
+                                [
+                                    h(
+                                        "Button",
+                                        {
+                                            props: {
+                                                type: "error",
+                                                size: "small",
+                                                ghost: true
+                                            },
+                                            style: {
+                                                margin: "5px"
+                                            }
+                                        },
+                                        "删除"
+                                    )
+                                ]
+                            )
+                        );
+
+                        return h("div", btnGroup);
+                    }
+                }
+            ],
+            data1: [
+                {
+                    id: 92,
+                    name: "Tw0igZ@LixsDlO^dbapcIUv%w*057wG]7U0*jHgem",
+                    workOrderNum: 522,
+                    shareNum: 374,
+                    updateAt: "2018-12-22 17:17:21",
+                    department: "101010101010101010101010101010",
+                    userId:
+                        "h63KMJc5l1I#wUERiQ#91Jbu5f7ti(pL*UYXJ0PNc5d(WcS67)7[DoaQPCd$1)^!S4*%smrj#8XGSu^4wk3*Pt3WLcZanLD4e9&j#[Qc[qf^Iezwi3bpoWzQ#Y@8x",
+                    userName: "ptIO",
+                    departmentName: "[NvxSH(Z]3",
+                    isleader: 1
+                },
+                {
+                    id: 101,
+                    name:
+                        "oWu]RPdl^&KAK7E@HDYD[s#weGQcI15&WJDOUgZo%#CL#nZQy@lB^mr4UPwg^#zoK7]irOgkOz1V5kFsIMrezh%K4N4amRgvKopgyplPQG6L@akSb3GA27sP9lW%T1P$zRGp*fKeJ^7#*9PYCR9[evTipamb83&fmcU4q&FWj2lt8w",
+                    workOrderNum: 717,
+                    shareNum: 433,
+                    updateAt: "2018-12-22 17:17:21",
+                    department:
+                        "1010101010101010101010101010101010101010101010101010",
+                    userId:
+                        "#kDlrwxTw@TG(yq3KBvojD8cMAigRRz#*uk[o*hsYVyCr%33&LoChOQ%M5UO#URB4NuRPh7Gd^O1557njgkmXWqUGwUjD9iC&8sLX))vj",
+                    userName: "Ls",
+                    departmentName: "rkd[dwIDx",
+                    isleader: 1
+                },
+                {
+                    id: 38,
+                    name:
+                        "hrS)yHn%cKqRqU9G%$H[nj&41Jary2imp[oVwwlugg0B!yRcJ72IJJKDJh7HCjJX$1sZ^2id*y5]3CfIRJ2VOpMNZ1!qQZ&Md[DQcqMIwz@Usi!16H0c6EJ%leVK&e!qGgnEtis$Yf4o[OBdmmH(negDr1w0hu",
+                    workOrderNum: 413,
+                    shareNum: 182,
+                    updateAt: "2018-12-22 17:17:21",
+                    department: "101010101010",
+                    userId:
+                        "kY[PCqCSNuyZUVcAehNKt8uPD(wos5Oq4r5T(t3Qu*F93fJ8pf6fx5VHC(U%im@CV9*7eNUADAkepNB*kB&fHgkn5(gg$Q%#j8EybXT7PP^",
+                    userName: "CO5D[Qrb4",
+                    departmentName: "$n1pPr",
+                    isleader: 1
+                }
+            ],
+            loading: false,
+            params: {
+                pageNum: 1,
+                pageSize: 10,
+                count: 0
+            }
+        };
+    }
+};
+</script>
