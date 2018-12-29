245e7862464d4d07537b1e6c46dae04fdabe1b37
520wsl <395548460@qq.com>
2018-12-29 14:39:49 +0800

    fix:工单案例库-跳转链接修改

--- src/admin/view/case/workOrderCaseInfo.vue
+++ src/admin/view/case/workOrderCaseInfo.vue
@@ -307,7 +307,7 @@
                                             }
                                         }
                                     },
-                                    "查看案例"
+                                    "查看工单"
                                 )
                             );
                             if (this.isDirector()) {
@@ -451,7 +451,7 @@
         created() {
             console.log(this.$route);
             this.name = this.$route.query.name || "";
-            this.params.caseLibraryId = this.$route.params.id || "";
+            this.params.caseLibraryId = this.$route.query.id || "";
         },
         mounted() {
             this.getList();
