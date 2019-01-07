a4c266cb7f929cc83800e530cba825ab38ea8384
520wsl <395548460@qq.com>
2019-01-07 10:40:34 +0800

    fix(工单模块): 判断是否领导bug、弹窗按钮关闭

--- src/api/admin/department/department.js
+++ src/api/admin/department/department.js
@@ -1,40 +1,43 @@
 import api from "@/libs/api.request";
+import {getArrMinNumBer} from "@/libs/util";
+
 const baseUrl = "/work-order/staff";
 
 function post(url, params) {
-  return api.post(baseUrl + url, params);
+    return api.post(baseUrl + url, params);
 }
 
 function get(url, params) {
-  return api.get(baseUrl + url, params);
+    return api.get(baseUrl + url, params);
 }
 
 export const getDepartmentData = () => {
-  return post("/department/listdepartment");
+    return post("/department/listdepartment");
 };
 export const getUserDepartmentData = () => {
     return post("/department/selectdepartment");
 };
-export const getUserData = ({ 
-    departmentId
- }) => {
-  return post("/department/selectuser", {
-    departmentId
-  });
+export const getUserData = ({
+                                departmentId
+                            }) => {
+    return post("/department/selectuser", {
+        departmentId
+    });
 };
 // 根据执行人部门Id查找上级领导
-export const getSuperiorLeader = ({ department }) => {
-  return post("/user/assigned", {
-    department
-  });
+export const getSuperiorLeader = ({department}) => {
+    return post("/user/assigned", {
+        department
+    });
 };
 // 是否是此部门领导
-export const getIsLeader = ({ 
-    sixiId, 
-    departmentId 
-    }) => {
+export const getIsLeader = ({
+                                sixiId,
+                                departmentId
+                            }) => {
+    let departmentIdMin = getArrMinNumBer(departmentId);
     return post("/department/isdepartmentleader", {
-        departmentId,
+        departmentId: departmentIdMin,
         sixiId
     });
-  };
+};
