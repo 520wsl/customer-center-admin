54a5f0a527cef8a68d5efa303993c46c09cc5201
520wsl <395548460@qq.com>
2019-01-22 10:34:34 +0800

    解决冲突

--- src/api/admin/user/sso.js
+++ src/api/admin/user/sso.js
@@ -1,16 +1,21 @@
 import api from "@/libs/api.request";
+
 const baseUrl = "/work-order";
+
 function post(url, params) {
-  return api.post(baseUrl + url, params);
+    return api.post(baseUrl + url, params);
 }
+
 function get(url, params) {
-  return api.get(baseUrl + url, params);
+    return api.get(baseUrl + url, params);
 }
+
 export const ssoCode = ({code}) => {
-	return post("/code",{code});
+    return post("/code", {code});
 };
 export const ssoLogin = () => {
-	return post("/login");
+    return '/api' + baseUrl + '/login'
+    // return post("/login");
 };
 
 
