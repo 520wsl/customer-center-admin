09ac820fd5f967e2bae8fefe84c688731d486a1f
2355456062@qq.com <2355456062@qq.com>
2019-01-25 10:00:30 +0800

    fix(工单模块): 代码优化

--- src/admin/view/workOrder/info.vue
+++ src/admin/view/workOrder/info.vue
@@ -698,6 +698,9 @@ export default {
             }
             getWorkorderPendingPerson(params).then(res=>{
                 if(res.status != 200){
+                    if(!this.isExectorId){
+                        return;
+                    }
                     this.$Modal.error({
                         title: "工单待我受理人员",
                         content: res.msg
@@ -726,8 +729,11 @@ export default {
                     }
                 })
             }).catch(error=>{
+                if(!this.isExectorId){
+                    return;
+                }
                 this.$Modal.error({
-                    title: "工单带我受理人员",
+                    title: "工单待我受理人员",
                     content: error.msg
                 });  
             })
