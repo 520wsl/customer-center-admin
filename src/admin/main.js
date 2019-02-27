e0a52f3e5b0f592ad7de87bdde2a527d41c83f1b
520wsl <395548460@qq.com>
2019-02-27 09:40:23 +0800

    feat(样式优化):添加 bootstrap 组件

--- src/admin/main.js
+++ src/admin/main.js
@@ -47,6 +47,7 @@ import {CDN, FILE} from "@/libs/tools";
 import "./index.less";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 
 Vue.use(iView);
