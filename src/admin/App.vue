0d1748f2c0f0b5a8dc2b48e0b3b92b6041de5e70
520wsl <395548460@qq.com>
2019-03-25 12:20:34 +0800

    feat(姐买你处理):增加水印

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -5,32 +5,35 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
-export default {
-    name: "app",
-    methods: {
-        _isMobile() {
-            let flag = navigator.userAgent.match(
-                /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-            );
-            return flag;
+    import {mapActions} from "vuex";
+    import Watermark from '@/assets/js/watermark'
+
+    export default {
+        name: "app",
+        methods: {
+            _isMobile() {
+                let flag = navigator.userAgent.match(
+                    /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+                );
+                return flag;
+            }
+        },
+        mounted() {
+            // if (this._isMobile()) {
+            //     alert("手机端");
+            // } else {
+            //     alert("pc端");
+            // }
+            Watermark.set("杭州四喜")
         }
-    },
-    mounted() {
-        // if (this._isMobile()) {
-        //     alert("手机端");
-        // } else {
-        //     alert("pc端");
-        // }
-    }
-};
+    };
 </script>
 
 <style>
-#app {
-    height: 100%;
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
+    #app {
+        height: 100%;
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 </style>
